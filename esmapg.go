a8da09e92337abfbdb59dbef1deed88006fb0e7d
ken <block24block@gmail.com>
2019-03-27 00:04:58 +0800

    implement belongs_to

--- esmapg.go
+++ esmapg.go
@@ -18,11 +18,36 @@ type fields struct {
 }
 
 func (m *Map) Sql() string {
-	return "SELECT " + m.onlySql() + " FROM " + m.Name
+	return "SELECT " + m.Fields.sql(m.Name) + " FROM " + m.Name
 }
 
-func (m *Map) onlySql() string {
-	return strings.Join(m.Fields.Only, ", ")
+func (fs *fields) sql(parentName string) string {
+	allSql := []string{fs.onlySql(), fs.belongsToSql(parentName)}
+
+	var sqls []string
+	for _, sql := range allSql {
+		if sql != "" {
+			sqls = append(sqls, sql)
+		}
+	}
+
+	return strings.Join(sqls, ", ")
+}
+
+func (fs *fields) onlySql() string {
+	return strings.Join(fs.Only, ", ")
+}
+
+func (fs *fields) belongsToSql(parentName string) string {
+	var sqls []string
+	for name, subFields := range fs.BelongsTo {
+		joining := parentName + "." + name + "_id = " + name + ".id"
+		sql := "( SELECT row_to_json(t) FROM ( SELECT " +
+			subFields.sql(name) + " FROM " + name + " WHERE " + joining +
+			") t ) AS " + name
+		sqls = append(sqls, sql)
+	}
+	return strings.Join(sqls, ", ")
 }
 
 func Parse(config []byte) []Map {
