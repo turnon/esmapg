fc865431e30c8c6de4d550554a87cceeef930e66
ken <block24block@gmail.com>
2019-03-27 22:06:42 +0800

    complete sql test case and move sql func into a seperate file

--- esmapg_test/esmapg_test.go
+++ esmapg_test/esmapg_test.go
@@ -1,23 +1,36 @@
 package esmapg_test
 
 import (
-	"fmt"
 	"io/ioutil"
+	"regexp"
 	"testing"
 
 	"github.com/turnon/esmapg"
 )
+
+var reRmSpaces = regexp.MustCompile(`\s|\n`)
 
 func jsonData() []byte {
 	json, _ := ioutil.ReadFile("./config.json")
 	return json
 }
 
+func sqlData() string {
+	sql, _ := ioutil.ReadFile("./select.sql")
+	return rmSpaces(string(sql))
+}
+
+func rmSpaces(str string) string {
+	return reRmSpaces.ReplaceAllString(str, ``)
+}
+
 func TestNew(t *testing.T) {
 	maps := esmapg.Parse(jsonData())
-	// fmt.Println(maps)
 
 	for _, m := range maps {
-		fmt.Println(m.SQL())
+		t.Log(m.SQL())
+		if rmSpaces(m.SQL()) != sqlData() {
+			t.Error("wrong sql")
+		}
 	}
 }
